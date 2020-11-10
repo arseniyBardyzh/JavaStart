377cd81ae1e059548eb7ac2760d27db39857b932
Arseni_Bardij <senia_94@mail.ru>
2020-11-10 08:45:43 +0300

    Задание 1 выполнено

---
+++ src/FirstDay.java
@@ -0,0 +1,24 @@
+import com.sun.security.jgss.GSSUtil;
+
+public class FirstDay {
+    public static void main(String[] args) {
+        /************
+         * Первое задание
+         **********/
+        System.out.println("Первое задание");
+        int i = 0;
+        String java;
+        while(i<10){
+            if (i<9){
+                System.out.printf("JAVA ");
+            }else{
+                System.out.printf("JAVA");
+            }
+            i++;
+        }
+        /************
+         * Второе задание
+         **********/
+        System.out.println("Второе задание");
+    }
+}
